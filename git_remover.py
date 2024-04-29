31ce5f1aa5a677df6dc9e5da595fcfbda15a1960
Elchinbek Jumanazarov <elchinbekjumanazarov@gmail.com>
2024-04-29 11:42:54 +0500

    create remove cmdline

--- git_remover.py
+++ git_remover.py
@@ -1,16 +1,17 @@
 
 import os
 import shutil
-def remover(path):
+from rich import print
+def remove_git(path):
+    print(path)
     for root, subFolder, files in os.walk(path):
         for folder in subFolder:
             if folder == '.git':
                 subfolder_path = os.path.join(root)
                 git_folder_path = os.path.join(root,folder)
-
-                print(git_folder_path)
+                print(f'[cyan]folder found: {git_folder_path}')
                 shutil.rmtree(subfolder_path, ignore_errors=True)
-                print(f'Removed {subfolder_path}')
+                print(f'[red]Removed {subfolder_path}')
 
 
-remover('C:/Users/Administrator/Desktop/test')
+
---
+++ main.py
@@ -0,0 +1,24 @@
+import click
+
+from git_remover import remove_git
+
+
+@click.group('remover')
+def remover():
+    pass
+
+
+
+@remover.command(help='Input .git foler path')
+@click.option('--path')
+def remove(path):
+    remove_git(path)
+    click.echo('Done')
+
+
+
+try:
+    if __name__ == '__main__':
+        remover()
+except Exception as e:
+    print(e)
